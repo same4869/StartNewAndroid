a21f57cc3a95e535caee81383e6174f6868614ed
xwang <xunwang@wenba100.com>
2016-11-21 17:48:02 +0800

    个人中心初步

--- StartNewAndroid/app/src/main/java/com/sna/xunwang/startnewandroid/activity/SNAMainActivity.java
+++ StartNewAndroid/app/src/main/java/com/sna/xunwang/startnewandroid/activity/SNAMainActivity.java
@@ -1,6 +1,5 @@
 package com.sna.xunwang.startnewandroid.activity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +10,7 @@ import com.jaeger.library.StatusBarUtil;
 import com.sna.xunwang.startnewandroid.R;
 import com.sna.xunwang.startnewandroid.fragment.BiezhiFragment;
 import com.sna.xunwang.startnewandroid.fragment.OtherFragment;
+import com.sna.xunwang.startnewandroid.fragment.UserFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,27 +34,24 @@ public class SNAMainActivity extends BaseActivity {
 
     private void initAHBottomNavigation() {
         // Create items
-        AHBottomNavigationItem item1 = new AHBottomNavigationItem("Label One", R.mipmap.tab_home,
-                parseColor("#455C65"));
-        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Label Two", R.mipmap.tab_second, Color
-                .parseColor("#00886A"));
-        AHBottomNavigationItem item3 = new AHBottomNavigationItem("Label Three", R.mipmap.tab_three,
-                Color.parseColor("#8B6B62"));
-        AHBottomNavigationItem item4 = new AHBottomNavigationItem("Label Four", R.mipmap.tab_four,
-                Color.parseColor("#008800"));
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem("好物推荐", R.mipmap.tab_home,
+                parseColor("#33bbff"));
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("暂时待定", R.mipmap.tab_second,
+                parseColor("#6e92C6"));
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("个人中心", R.mipmap.tab_three,
+                parseColor("#6e92C6"));
 
         // Add items
         bottomNavigation.addItem(item1);
         bottomNavigation.addItem(item2);
         bottomNavigation.addItem(item3);
-        bottomNavigation.addItem(item4);
 
         // Set background color
-        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
+        bottomNavigation.setDefaultBackgroundColor(parseColor("#FEFEFE"));
 
         // Change colors
-        bottomNavigation.setAccentColor(Color.parseColor("#F63D2B"));
-        bottomNavigation.setInactiveColor(Color.parseColor("#747474"));
+        bottomNavigation.setAccentColor(parseColor("#F63D2B"));
+        bottomNavigation.setInactiveColor(parseColor("#747474"));
 
         // Use colored navigation with circle reveal effect
         bottomNavigation.setColored(true);
@@ -63,7 +60,7 @@ public class SNAMainActivity extends BaseActivity {
         bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
             @Override
             public void onTabSelected(int position, boolean wasSelected) {
-                showFragment(fragments.get(position % 2));
+                showFragment(fragments.get(position % 3));
 //                ToastUtil.showToast(getApplicationContext(), "position --> " + position);
             }
         });
@@ -73,6 +70,7 @@ public class SNAMainActivity extends BaseActivity {
     public void initViews(Bundle savedInstanceState) {
         fragments.add(new BiezhiFragment());
         fragments.add(new OtherFragment());
+        fragments.add(new UserFragment());
 
         showFragment(fragments.get(0));
         initAHBottomNavigation();
---
+++ StartNewAndroid/app/src/main/java/com/sna/xunwang/startnewandroid/fragment/UserFragment.java
@@ -0,0 +1,24 @@
+package com.sna.xunwang.startnewandroid.fragment;
+
+import com.sna.xunwang.startnewandroid.R;
+
+/**
+ * Created by xunwang on 16/11/21.
+ */
+
+public class UserFragment extends BaseFragment{
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_user;
+    }
+
+    @Override
+    public void lazyFetchData() {
+
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+}
